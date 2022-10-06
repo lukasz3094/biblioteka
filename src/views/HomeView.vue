2070b863b1bcc687b74133acfcc2386219531a40
Luke_127 <74059306+lukasz3094@users.noreply.github.com>
2022-10-06 16:25:31 +0200

    Change homeview
    Connect homeview with login/logout system for users

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,39 @@
 <template>
-	<div class="container">
-		<img alt="Vue logo" src="../assets/logo.png">
+	<div class="container w-75 p-1">
+		<h1 class="text-center m-1">Hi {{ username }}</h1>
+		<p class="text-center">{{ secretMessage }}</p>
+		<div class="row justify-content-center">
+			<div class="col-2">
+				<button class="btn btn-info p-2 text-uppercase" @click="logout" >
+					Wyloguj
+				</button>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
+import AuthService from "@/services/AuthService"
 
 export default {
-	name: 'HomeView',
+	data () {
+		return {
+			secretMessage: "",
+			username: ""
+		}
+	},
+	async created() {
+		if (!this.$store.getters.isLoggedIn) {
+			this.$router.push("/login")
+		}
+		this.username = this.$store.getters.getUser.username
+		this.secretMessage = await AuthService.getSecretContent()
+	},
+	methods: {
+		logout() {
+			this.$store.dispatch("logout")
+			this.$router.push("/login")
+		}
+	}
 }
 </script>
