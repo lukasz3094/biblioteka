97147707ac73c5392732b711c0851fff2b2f0760
Luke_127 <74059306+lukasz3094@users.noreply.github.com>
2022-10-09 14:10:17 +0200

    Obsługa logowania w osobnym pliku

---
+++ src/services/AdminAuthService.js
@@ -0,0 +1,16 @@
+import Axios from "axios"
+
+const url = "http://localhost:3500/admin/"
+
+export default {
+	async login (credentials) {
+		const response = await Axios
+			.post(url + "login/", credentials)
+		return response.data
+	},
+	// async signUp (credentials) {
+	// 	const response = await Axios
+	// 		.post(url + "sign-up/", credentials)
+	// 	return response.data
+	// },
+}
